a551aef0c9ff2852be629abcfb5dac4a54c2fb98
DamienCourcoux <damsmailspro@gmail.com>
2021-12-07 17:35:46 +0100

    Simon

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@
  */
 const app = {
   // just a utility var to remember all the colors
-  colors: ['red','green','blue','yellow'],
+  colors: ['red', 'green', 'blue', 'yellow'],
 
   // this var will contain the sequence said by Simon
   sequence: [],
+  indice: 0,
 
   drawCells: function () {
     const playground = document.getElementById('playground');
@@ -24,7 +25,7 @@ const app = {
     // let's modify the syle directly
     document.getElementById(color).style.borderWidth = '45px';
     // and reset the same style, after a small pause (150 ms)
-    setTimeout( () => {
+    setTimeout(() => {
       document.getElementById(color).style.borderWidth = '0';
     }, 150);
 
@@ -33,12 +34,13 @@ const app = {
   newGame: function () {
     // start by reseting the sequence 
     app.sequence = [];
+    app.indice = 0;
     // make it 3 times :
     for (let index = 0; index < 3; index++) {
       // get a random number between 0 and 3
-      let random = Math.floor(Math.random()*4);
+      let random = Math.floor(Math.random() * 4);
       // add the corresponding color to the sequence
-      app.sequence.push( app.colors[random] );
+      app.sequence.push(app.colors[random]);
     }
     app.showMessage("La partie commence, bonne chance !");
 
@@ -48,19 +50,27 @@ const app = {
 
   simonSays: function (sequence) {
     if (sequence && sequence.length) {
+      app.stopListenClickEvents();
+      app.stopTimeout();
+      app.showMessage("Mémorisez la séquence");
       // after 500ms, bump the first cell
-      setTimeout( app.bumpCell, 500, sequence[0] );
+      setTimeout(app.bumpCell, 500, sequence[0]);
       // plays the rest of the sequence after a longer pause
-      setTimeout( app.simonSays, 850, sequence.slice(1));
+      setTimeout(app.simonSays, 850, sequence.slice(1));
+    } else {
+      app.showMessage("Reproduisez la séquence");
+      app.startTimeout();
+      app.listenClickEvents();
     }
   },
 
   init: function () {
     console.log('init');
     app.drawCells();
+    app.listenClickEvents()
 
     // listen click on the "go" button
-    document.getElementById('go').addEventListener('click', app.newGame );
+    document.getElementById('go').addEventListener('click', app.newGame);
   },
 
   /** Fin du code fourni. Après, c'est à toi de jouer! */
@@ -82,6 +92,51 @@ const app = {
     alert(`Partie terminée. Votre score: ${app.sequence.length}`);
     app.showBtnStart();
     app.sequence = [];
+  },
+
+  listenClickEvents: function () {
+    const cellTargets = document.querySelectorAll(".cell");
+    for (const cellTarget of cellTargets) {
+      cellTarget.addEventListener("click", app.onCellClick)
+    }
+  },
+
+  stopListenClickEvents: function () {
+    const cellTargets = document.querySelectorAll(".cell");
+    for (const cellTarget of cellTargets) {
+      cellTarget.removeEventListener("click", app.onCellClick)
+    }
+  },
+
+  onCellClick: function (evt) {
+    app.stopTimeout();
+    app.startTimeout();
+    const clickedColor = evt.target.id;
+    app.bumpCell(clickedColor);
+    if (clickedColor !== app.sequence[app.indice]) {
+      app.endGame();
+    } else {
+      if (app.indice === app.sequence.length - 1) {
+        app.nextMove();
+      } else {
+        app.indice += 1;
+      }
+    }
+  },
+
+  nextMove: function () {
+    const colorIndex = Math.floor(Math.random() * 4);
+    app.sequence.push(app.colors[colorIndex]);
+    app.indice = 0;
+    app.simonSays(app.sequence);
+  },
+
+  startTimeout: function () {
+    app.timeoutRef = setTimeout(app.endGame, 5000);
+  },
+
+  stopTimeout: function () {
+    clearTimeout(app.timeoutRef);
   }
 
 };
